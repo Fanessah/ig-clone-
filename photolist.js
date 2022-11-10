ef76695039ef26ad2fd32e6429eb09f8bb335132
Fanessa <fanessah@gmail.com>
2022-11-10 10:53:23 -0500

    created ig clone

---
+++ photolist.js
@@ -0,0 +1,45 @@
+//put a fake list of photo instead off hitting an API 
+
+
+const photolist = [
+    {id:1,
+    photoURL: 'https://images.nappy.co/uploads/large/c036f420-188e-46a3-9eeb-076d5a9380f5-34111609776231bf5dlglzhxnomxdcunjrjsjervujthigeynbqkkd4ahhlvcbnsdki3imhgqd44dvsrx6trohcqzmt8now5r8pwfovxxoytkq9xsc.jpeg?auto=format&w=960&q=75',
+    description : "woman posing yoga",
+    userName:'yogi_4eva',
+    profilePhoto:'https://randomuser.me/api/portraits/women/1.jpg',
+
+    },
+    {id:2,
+    photoURL: 'https://nappy.co/photo/836',
+    description : "woman wearing face mask",
+    userName:'hyper_aware',
+    profilePhoto:'https://randomuser.me/api/portraits/women/2.jpg',
+    },
+    {id:3,
+    photoURL: 'https://nappy.co/photo/276',
+    description : "Man in plain suit drinking coffee",
+    userName:'coffeefanatic',
+    profilePhoto:'https://randomuser.me/api/portraits/men/3.jpg',
+    },
+    {id:4,
+    photoURL: 'https://nappy.co/photo/503',
+    description : "Pregnant black woman wearing sweatshirt that says pregnant AF ",
+    userName:'mommy2B',
+    profilePhoto:'https://randomuser.me/api/portraits/women/4.jpg',
+    },
+    {id:5,
+    photoURL: 'https://nappy.co/photo/153',
+    description : "Black men laughing in a group",
+    userName:'joyouspeeps',
+    profilePhoto:'https://randomuser.me/api/portraits/men/5.jpg',
+    },
+    {id:6,
+    photoURL: 'https://nappy.co/photo/219',
+    description : "Somebody working on an analysis report ",
+    userName:'datalover',
+    profilePhoto:'https://randomuser.me/api/portraits/women/6.jpg',
+    },
+]
+
+export default photolist
+ 
